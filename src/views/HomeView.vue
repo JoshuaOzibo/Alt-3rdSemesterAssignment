0977d62198badfa01d1569ede4061649a3639ace
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2024-05-27 13:06:34 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import {FetchRepo} from '../Composables/UseFetch.js';
+</script>
+
+<template>
+ <h1>Hello From Home</h1>
+</template>
---
+++ src/views/SingleRepo.vue
@@ -0,0 +1,9 @@
+<style setup>
+
+</style>
+
+<template>
+  <div class="about">
+    <h1 class="text-2xl bg-red-300">This is an about page</h1>
+  </div>
+</template>
